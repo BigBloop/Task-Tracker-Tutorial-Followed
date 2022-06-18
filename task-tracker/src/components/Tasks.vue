7b098932cd7ae9fc1fc9134d203f14b9c5d4d496
BigBloop <ansomofficial@gmail.com>
2022-06-18 23:14:56 +0800

    added files and components

---
+++ task-tracker/src/components/Tasks.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div v-for="(task, tasks) in tasks" :key="tasks">
+            <Task @delete-task="$emit('delete-task', task.id)" :task="task" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import Task from './Task'
+    export default {
+        name: 'TasksItem',
+        props: {
+            tasks: Array,
+        },
+        components: {
+            Task
+        },
+        emits: ['delete-task'],
+    }
+</script>
